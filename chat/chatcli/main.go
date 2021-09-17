06081e7392101a6c594e41ced6218ae41b963213
nj-eka <njeka0108@gmail.com>
2021-09-17 15:45:43 +0300

    v0.0.1

--- chat/chatcli/main.go
+++ chat/chatcli/main.go
@@ -11,33 +11,37 @@ import (
 )
 
 func main() {
-	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 	d := net.Dialer{
 		Timeout:   cfg.Timeout,
 		KeepAlive: cfg.KeepAlive,
 	}
-	conn, err := d.DialContext(ctx,  cfg.Protocol, cfg.Address)
+	conn, err := d.DialContext(ctx, cfg.Protocol, cfg.Address)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("client [pid: %d] connected to chat server: %s -> %s ", os.Getpid(), conn.LocalAddr(), conn.RemoteAddr())
-	go func(){ // force conn closing
-		<-ctx.Done()
-		if err := conn.Close(); err != nil{
+	go func() {
+		// if conn.Closed => Copy exit with error - ok
+		io.Copy(os.Stdout, conn)
+	}()
+	select {
+	case <-ctx.Done():
+		if err := conn.Close(); err != nil {
 			log.Println("closing conn failed: ", err)
 		} else {
 			log.Println("conn closed")
 		}
-	}()
-	go func() {
-		io.Copy(os.Stdout, conn)
-	}()
-	<- func() <-chan struct{} {
+	case <-func() <-chan struct{} {
 		done := make(chan struct{})
-		go func(){
+		go func() {
+			// doc: Copy copies from src to dst until either EOF is reached on src or an error occurs.
+			// os.Stdin never ends with EOF here
 			_, _ = io.Copy(conn, os.Stdin)
 			close(done)
 		}()
 		return done
-	}()
+		}():
+	}
 }
